51bc5efbaf8d977a5c8ac154410afc3d45761805
Paul Guilbert <paul.guilbert@backmarket.com>
2024-12-11 15:33:52 +0100

    fix: throw when a store is used outside of a Nuxt-aware context.
    Prefer the Nuxt Pinia instance over the global active Pinia instance. Since the Nuxt Pinia instance is discarded after each request, it ensures that we can't accidentally use one from another request. Additionally, `usePinia` will throw an error when used outside of a Nuxt-aware context.
    
    The error is as follows in dev :
    > [nuxt] A composable that requires access to the Nuxt instance was called outside of a plugin, Nuxt hook, Nuxt middleware, or Vue setup function.

--- packages/nuxt/src/runtime/composables.ts
+++ packages/nuxt/src/runtime/composables.ts
@@ -1,4 +1,27 @@
 import { useNuxtApp } from '#app'
+import {
+  defineStore as _defineStore,
+  type Pinia,
+  type StoreGeneric,
+} from 'pinia'
 export * from 'pinia'
 
 export const usePinia = () => useNuxtApp().$pinia
+
+export const defineStore = (...args) => {
+  if (!import.meta.server) {
+    return _defineStore(...args)
+  }
+
+  const store = _defineStore(...args)
+
+  function useStore(pinia?: Pinia | null, hot?: StoreGeneric): StoreGeneric {
+    if (pinia) {
+      return store(pinia, hot)
+    }
+
+    return store(usePinia(), hot)
+  }
+
+  return useStore
+}
